12e85805aa0255af46dae23ae2c9e6a53d853641
sherin-V <sherinv.de@gmail.com>
2026-02-04 11:28:59 +0100

    Feature: Added Mobile Hamburger Menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useTheme } from '../context/ThemeContext';
 import './Navbar.css';
 
 const Navbar = () => {
     const { currentTheme } = useTheme();
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => setIsOpen(!isOpen);
 
     return (
         <nav className="isu-navbar" style={{ color: currentTheme.colors.text }}>
-            <Link to="/" className="isu-nav-logo" style={{ color: currentTheme.colors.text }}>ISU</Link>
-            <div className="isu-nav-links">
-                <Link to="/" className="isu-nav-link" style={{ color: currentTheme.colors.text }}>Home</Link>
-                <Link to="/about" className="isu-nav-link" style={{ color: currentTheme.colors.text }}>About</Link>
-                <Link to="/community" className="isu-nav-link" style={{ color: currentTheme.colors.text }}>Community</Link>
-                <Link to="/login" className="isu-nav-link" style={{ color: currentTheme.colors.text }}>Login</Link>
+            <div className="isu-nav-header">
+                <Link to="/" className="isu-nav-logo" style={{ color: currentTheme.colors.text }}>ISU</Link>
+                <div className="isu-hamburger" onClick={toggleMenu}>
+                    <span style={{ background: currentTheme.colors.text }}></span>
+                    <span style={{ background: currentTheme.colors.text }}></span>
+                    <span style={{ background: currentTheme.colors.text }}></span>
+                </div>
+            </div>
+
+            <div className={`isu-nav-links ${isOpen ? 'active' : ''}`}>
+                <Link to="/" className="isu-nav-link" onClick={() => setIsOpen(false)} style={{ color: currentTheme.colors.text }}>Home</Link>
+                <Link to="/about" className="isu-nav-link" onClick={() => setIsOpen(false)} style={{ color: currentTheme.colors.text }}>About</Link>
+                <Link to="/community" className="isu-nav-link" onClick={() => setIsOpen(false)} style={{ color: currentTheme.colors.text }}>Community</Link>
+                <Link to="/login" className="isu-nav-link" onClick={() => setIsOpen(false)} style={{ color: currentTheme.colors.text }}>Login</Link>
             </div>
         </nav>
     );
